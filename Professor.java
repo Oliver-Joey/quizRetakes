1fedb94a215aa45522a925f5510d2ffaf0564828
schierlingJ1 <42974760+schierlingJ1@users.noreply.github.com>
2019-03-07 09:59:21 -0500

    Add files via upload
    added method functionality

--- Professor.java
+++ Professor.java
@@ -3,6 +3,8 @@ package quizretakes;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.*;
+
 
 public class Professor {
     private static final String apptsBase   = "quiz-appts";
@@ -13,30 +15,108 @@ public class Professor {
     }
 
     public static void displayRetakes(String courseID) {
-     Scanner scan = new Scanner(System.in);
-     String in = dataLocation + apptsBase + "-" + courseID + ".txt";
-     File input = new File(in);
+        Scanner scan = new Scanner(System.in);
+        String in = dataLocation + apptsBase + "-" + courseID + ".txt";
+        File input = new File(in);
 
-     try {
-       System.out.println("Here is the current schedule of quiz retakes:\n");
-       Scanner fileScan = new Scanner(input);
-       int count = 1;
-       while (fileScan.hasNextLine()) {
-         String retake = fileScan.nextLine();
-         System.out.println(count + ". " + retake);
-         count++;
-       }
-       System.out.println("\nHave a nice day!");
-       } catch (FileNotFoundException e){
-         System.out.println("Oops I couldn't read the file...");
-       }
+        try {
+            System.out.println("Here is the current schedule of quiz retakes:\n");
+            Scanner fileScan = new Scanner(input);
+            int count = 1;
+            while (fileScan.hasNextLine()) {
+                String retake = fileScan.nextLine();
+                System.out.println(count + ". " + retake);
+                count++;
+            }
+            System.out.println("\nHave a nice day!");
+        } catch (FileNotFoundException e){
+            System.out.println("Oops I couldn't read the file...");
+        }
     }
-    
-    public static void scheduleQuiz(String courseID){
-      
+
+    public static void scheduleQuiz() {
+        Scanner scan = new Scanner(System.in);
+        String newQuiz = "";
+
+        System.out.print("What\'s the quiz ID?\n> ");
+        String quizID = scan.nextLine();
+        newQuiz += ("quizID: " + quizID + ", ");
+
+        System.out.print("What\'s the quiz date?\n> ");
+        String date = scan.nextLine();
+        newQuiz += ("date: " + date + ", ");
+
+        System.out.print("What\'s the quiz time?\n> ");
+        String time = scan.nextLine();
+        newQuiz += ("time: " + time + ", ");
+
+        System.out.print("What\'s the quiz location?\n> ");
+        String location = scan.nextLine();
+        newQuiz += ("location: " + location);
+
+        System.out.println("\nAll set! Here is your new quiz:\n" + newQuiz);
+
+        writeToFile("quizzes", newQuiz);
     }
-    
-    public static void scheduleRetake(String courseID){
-      
+
+    public static void scheduleRetake() {
+        Scanner scan = new Scanner(System.in);
+        String newRetake = "";
+
+        System.out.print("What\'s the retake ID?\n> ");
+        String retakeID = scan.nextLine();
+        newRetake += ("quizID: " + retakeID + ", ");
+
+        System.out.print("What\'s the retake location?\n> ");
+        String location = scan.nextLine();
+        newRetake += ("location: " + location + ", ");
+
+        System.out.print("What\'s the retake month?\n> ");
+        String month = scan.nextLine();
+        newRetake += ("month: " + month + ", ");
+
+        System.out.print("What\'s the retake day?\n> ");
+        String day = scan.nextLine();
+        newRetake += ("day: " + day + ", ");
+
+        System.out.print("What\'s the retake hour?\n> ");
+        String hour = scan.nextLine();
+        newRetake += ("hour: " + hour + ", ");
+
+        System.out.print("What\'s the retake minute?\n> ");
+        String minute = scan.nextLine();
+        newRetake += ("minute: " + minute);
+
+        System.out.println("\nAll set! Here is your new retake:\n" + newRetake);
+
+        //write to file
+        writeToFile("retakes", newRetake);
+    }
+
+    public static void writeToFile(String form, String data) {
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter((form+".txt")));
+            out.write(data);
+            out.close();
+        } catch (IOException e){
+            System.out.println("Oops I couldn't find " + form);
+        }
+    }
+
+    public static void readFile(String form) {
+        System.out.println("\nReading " + form + ":");
+        int count = 0;
+
+        try {
+            Scanner scan = new Scanner(new File((form+".txt")));
+            while (scan.hasNextLine()) {
+                String retake = scan.nextLine();
+                System.out.println(count + ": " + retake);
+                count++;
+            }
+            System.out.println("\nReading complete.");
+        } catch (FileNotFoundException e){
+            System.out.println("Oops I couldn't read " + form);
+        }
     }
 }
